c067cfdb5e4f2775219d2e55e04f9a388cee29ab
Desi Parunova <desi.parunova@gmail.com>
2026-01-18 13:43:20 +0200

    Added code

---
+++ code2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    
+    printf("Enter a:");
+    scanf("%d", &a);
+    
+    printf("Enter b:");
+    scanf("%d", &b);
+    
+    int sum = a + b;
+    printf("The sum is: %d", sum);
+    
+    return 0;
+}
